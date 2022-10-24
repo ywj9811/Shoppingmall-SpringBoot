5f43ac6adbb2db171c0a85f250236636a3f760d6
ywj9811 <ywj9811@naver.com>
2022-10-25 01:56:53 +0900

    설정

--- src/main/java/sb/shoppingmall/config/ShopConfig.java
+++ src/main/java/sb/shoppingmall/config/ShopConfig.java
@@ -3,9 +3,13 @@ package sb.shoppingmall.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import sb.shoppingmall.repository.CartRepository;
 import sb.shoppingmall.repository.UserRepository;
+import sb.shoppingmall.repository.mybatis.CartMapper;
+import sb.shoppingmall.repository.mybatis.MyBatisCartRepository;
 import sb.shoppingmall.repository.mybatis.MyBatisUserRepository;
 import sb.shoppingmall.repository.mybatis.UserMapper;
+import sb.shoppingmall.service.CartService;
 import sb.shoppingmall.service.UserService;
 
 @Configuration
@@ -13,9 +17,11 @@ import sb.shoppingmall.service.UserService;
 public class ShopConfig {
 
     private final UserMapper userMapper;
+    private final CartMapper cartMapper;
 
     /**
      * 여기에 빈 등록과 매퍼 등록을 하자
+     * 기술이 변하면 빠르게 변경할 수 있도록
      */
 
     @Bean
@@ -26,5 +32,15 @@ public class ShopConfig {
     @Bean
     public UserService userService(){
         return new UserService(userRepository());
+    }
+
+    @Bean
+    public CartRepository cartRepository(){
+        return new MyBatisCartRepository(cartMapper);
+    }
+
+    @Bean
+    public CartService cartService(){
+        return new CartService(cartRepository());
     }
 }
