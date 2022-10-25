32ac9c0a25b6049b8788c0e1542144318a273612
ywj9811 <ywj9811@naver.com>
2022-10-26 00:06:14 +0900

    빈 등록

--- src/main/java/sb/shoppingmall/config/ShopConfig.java
+++ src/main/java/sb/shoppingmall/config/ShopConfig.java
@@ -3,14 +3,9 @@ package sb.shoppingmall.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import sb.shoppingmall.repository.CartRepository;
-import sb.shoppingmall.repository.UserRepository;
-import sb.shoppingmall.repository.mybatis.CartMapper;
-import sb.shoppingmall.repository.mybatis.MyBatisCartRepository;
-import sb.shoppingmall.repository.mybatis.MyBatisUserRepository;
-import sb.shoppingmall.repository.mybatis.UserMapper;
-import sb.shoppingmall.service.CartService;
-import sb.shoppingmall.service.UserService;
+import sb.shoppingmall.repository.*;
+import sb.shoppingmall.repository.mybatis.*;
+import sb.shoppingmall.service.*;
 
 @Configuration
 @RequiredArgsConstructor
@@ -18,7 +13,9 @@ public class ShopConfig {
 
     private final UserMapper userMapper;
     private final CartMapper cartMapper;
-
+    private final OrderMapper orderMapper;
+    private final PaymentMapper paymentMapper;
+    private final OrderListMapper orderListMapper;
     /**
      * 여기에 빈 등록과 매퍼 등록을 하자
      * 기술이 변하면 빠르게 변경할 수 있도록
@@ -42,5 +39,35 @@ public class ShopConfig {
     @Bean
     public CartService cartService(){
         return new CartService(cartRepository());
+    }
+
+    @Bean
+    public OrderRepository orderRepository(){
+        return new MyBatisOrderRepository(orderMapper);
+    }
+
+    @Bean
+    public OrderService orderService() {
+        return new OrderService(orderRepository());
+    }
+
+    @Bean
+    public PaymentRepository paymentRepository(){
+        return new MyBatisPaymentRepository(paymentMapper);
+    }
+
+    @Bean
+    public PaymentService paymentService() {
+        return new PaymentService(paymentRepository());
+    }
+
+    @Bean
+    public OrderListRepository orderListRepository() {
+        return new MyBatisOrderListRepository(orderListMapper);
+    }
+
+    @Bean
+    public OrderListService orderListService() {
+        return new OrderListService(orderListRepository());
     }
 }
