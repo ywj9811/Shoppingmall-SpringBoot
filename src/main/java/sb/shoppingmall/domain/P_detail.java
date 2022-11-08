57ee8163aa503be9e20fdb6408e0b7f89f9986a7
ywj9811 <ywj9811@naver.com>
2022-11-09 01:14:53 +0900

    feat: 컬럼 추가

--- src/main/java/sb/shoppingmall/domain/P_detail.java
+++ src/main/java/sb/shoppingmall/domain/P_detail.java
@@ -5,5 +5,5 @@ import lombok.Data;
 @Data
 public class P_detail {
     private Long p_info_dcode, p_info_code, user_code;
-    private int p_detail_cell;
+    private int p_detail_cell, p_info_price;
 }
